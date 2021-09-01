357ec0a18d3e2c6b0cb0372e897fe9753411f0e3
PRIYANSHU GUPTA <74448032+priyanshuguptaiitg@users.noreply.github.com>
2021-09-01 16:41:55 +0530

    Delete btc directory

--- btc/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-$host = 'localhost';
-$user = 'root';
-$pass = '';
-$database = 'BIT_PE';
-$conn = mysqli_connect($host, $user, $pass, $database) or die("<script>alert('Connection Failed due to mysqli_connect_error()')</script>");
--- btc/php/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$servername = "localhost";
-	$username = "root";
-	$password = "";
-	$dbname = "database";
-	$conn = mysqli_connect($servername, $username, $password, $dbname) or die("DB NOT CONNECTED, Error : --> ".mysqli_connect_error());
-?>
