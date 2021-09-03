5487030ed5b6813866b543d07a62b13eb29220e6
PRIYANSHU GUPTA <74448032+priyanshuguptaiitg@users.noreply.github.com>
2021-09-03 12:40:23 +0530

    Delete btc directory

--- btc/php/footer.php
+++
@@ -1,4 +0,0 @@
-  <?php echo "
-  <footer class='text-center mt-5 py-2'><b>&copy; PRIYANSHU GUPTA <script>
-        document.write(new Date().getFullYear())
-      </script>. All Rights Reserved</b></footer>" ?>
